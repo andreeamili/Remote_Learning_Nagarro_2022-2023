4a26fd5df21fd0bb7744805072ca1ff6a5e9740c
Militaru Andreea <andreea.mili20@gmail.com>
2023-03-15 09:49:31 +0200

    UnitTest for Terrra

---
+++ Terra/Terra/Country.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+
+namespace iQuest.Terra
+{
+    public class Country : IComparable, IEquatable<Country>
+    {
+        public string Name { get; }
+
+        public string Capital { get; }
+
+        public Country(string name, string capital)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Capital = capital ?? throw new ArgumentNullException(nameof(capital));
+        }
+        public bool Equals(Country obj)
+        {
+
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Country))
+            {
+                return false;
+            }
+
+            var person = obj as Country;
+
+            return this.Name == person.Name && this.Capital == person.Capital;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return Equals(obj as Country);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode()^this.Capital.GetHashCode();
+        }
+
+        public int CompareTo(object obj)
+        {
+           if(obj==null)
+            {
+                return 1;
+            }
+
+            Country country=obj as Country;
+            if (country == null)
+            {
+                throw new ArgumentException("This is not a country");
+            }
+
+            int result=Name.CompareTo(country.Name);
+            if (result == 0)
+            {
+                result = Capital.CompareTo(country.Capital);
+            }
+            return result;
+        }
+
+
+    }
+}
---
+++ Terra/TerraPlus/ContinentPlus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using iQuest.Terra;
+
+namespace iQuest.TerraPlus
+{
+    public class ContinentPlus 
+    {
+        private readonly List<Country> countries = new List<Country>();
+
+        public ContinentPlus()
+        {
+        }
+
+        public ContinentPlus(IEnumerable<Country> countries)
+        {
+            if (countries == null) throw new ArgumentNullException(nameof(countries));
+
+            this.countries.AddRange(countries);
+        }
+
+        public IEnumerable<Country> EnumerateCountriesByName()
+        {
+            countries.Sort();
+            return countries;
+        }
+
+        public IEnumerable<Country> EnumerateCountriesByCapital()
+        {
+            return countries.OrderBy(c => c?.Capital, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
+
